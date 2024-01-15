64c0d13d20dd5dcde56dc5af1f3bd94b43aebcc1
MustafaAswadi <aswadimustafa16@gmail.com>
2024-01-15 22:28:10 +0430

    create author service

---
+++ src/authors/authors.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AuthorEntity } from './datastore/authors.entity';
+import { Repository } from 'typeorm';
+import { MapEntityToDomain } from './datastore/entity-to-domain.mapper';
+import { AuthorDomain } from './authors.domain';
+
+@Injectable()
+export class AuthorsService {
+  constructor(
+    @InjectRepository(AuthorEntity)
+    private readonly authorRepository: Repository<AuthorEntity>,
+  ) {}
+
+  async findAll(): Promise<AuthorDomain[]> {
+    const res = await this.authorRepository.find();
+    return res.map(MapEntityToDomain);
+  }
+
+  async findOne(id: string): Promise<AuthorDomain> {
+    const res = await this.authorRepository.findOne({ where: { id } });
+    return MapEntityToDomain(res);
+  }
+
+  async create(author: AuthorDomain): Promise<AuthorDomain> {
+    const res = await this.authorRepository.save(author);
+    return MapEntityToDomain(res);
+  }
+
+  async update(author: AuthorDomain): Promise<AuthorDomain> {
+    const res = await this.authorRepository.save(author);
+    return MapEntityToDomain(res);
+  }
+
+  async remove(id: string): Promise<boolean> {
+    await this.authorRepository.delete({ id });
+    return true;
+  }
+}
